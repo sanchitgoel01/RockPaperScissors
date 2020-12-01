77e42dfcf8ebffd9abd529515d2678cf353449d5
Sanchit Goel <sanchitgoel@hotmail.com>
2020-12-01 11:54:34 -0800

    Fix path issue with github pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 // vue.config.js
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/RockPaperScissors/'
+    : '/',
     chainWebpack: config => {
         config
             .plugin('html')
